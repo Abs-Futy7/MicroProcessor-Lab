1b5c8720eb174025d96d9bcb1fd0c1e1c86ce974
Abs-Futy7 <abubakarsiddique01631172611@gmail.com>
2025-08-27 22:09:43 +0600

    lab 1 added

---
+++ Lab1/hello.asm
@@ -0,0 +1,51 @@
+; Import the printf function from the C standard library
+extern printf
+
+; SECTION .data starts the data segment for variable declarations
+SECTION .data
+a: dq 5          ; Declare 64-bit integer 'a' initialized to 5
+b: dq 2          ; Declare 64-bit integer 'b' initialized to 2
+c: dq 0          ; Declare 64-bit integer 'c' initialized to 0 (will hold sum)
+fmt: db "a=%ld, b=%ld c=%ld", 10, 0 ; Format string for printf with newline
+
+; SECTION .text starts the code segment
+SECTION .text
+global main      ; Make 'main' visible to linker
+
+main:
+    push rbp         ; Save base pointer (function prologue)
+    mov rax,[a]      ; Load value of 'a' into rax
+    mov rbx,[b]      ; Load value of 'b' into rbx
+    add rax,rbx      ; Add rbx (b) to rax (a), result in rax
+    mov [c],rax      ; Store result (a+b) into 'c'
+    mov rdi,fmt      ; First argument to printf: address of format string
+    mov rsi,[a]      ; Second argument: value of 'a'
+    mov rdx,[b]      ; Third argument: value of 'b'
+    mov rcx,[c]      ; Fourth argument: value of 'c'
+    mov rax,0        ; Clear rax (required for variadic functions on Linux)
+    call printf      ; Call printf to print the values
+    pop rbp          ; Restore base pointer (function epilogue)
+    mov rax,0        ; Return 0 from main
+    ret              ; Return to caller
+SECTION .data
+a: dq 5
+b: dq 2
+c: dq 0
+fmt: db "a=%ld, b=%ld c=%ld", 10, 0
+SECTION .text
+global main
+main:
+push rbp
+mov rax,[a]
+mov rbx,[b]
+add rax,rbx
+mov [c],rax
+mov rdi,fmt
+mov rsi,[a]
+mov rdx,[b]
+mov rcx,[c]
+mov rax,0
+call printf
+pop rbp
+mov rax,0
+ret
